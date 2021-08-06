a5804cc568096960200b74b1a5c4abab906b5c25
Remi Rousselet <darky12s@gmail.com>
2021-08-06 16:16:54 +0100

    WIP extends

--- packages/freezed_annotation/lib/freezed_annotation.dart
+++ packages/freezed_annotation/lib/freezed_annotation.dart
@@ -185,48 +185,65 @@ class Default {
   final Object? defaultValue;
 }
 
-/// Marks a union type to implement the interface [type] or [stringType].
-/// In the case below `City` will implement `GeographicArea`.
+/// Marks a union type to implement a type.
+///
 /// ```dart
 /// @freezed
 /// abstract class Example with _$Example {
 ///   const factory Example.person(String name, int age) = Person;
 ///
-///   @Implements(GeographicArea)
-///   const factory Example.city(String name, int population) = City;
-/// }
-/// ```
-///
-/// In case you need to implement a generic class, due to the fact that
-/// annotations can't have type arguments, you must use the
-/// [Implements.fromString(stringType)] constructor and provide the type as a
-/// String.
-/// ```dart
-/// @freezed
-/// abstract class Example with _$Example {
-///   const factory Example.person(String name, int age) = Person;
-///
-///   @Implements.fromString('AdministrativeArea<House>')
-///   const factory Example.street(String name) = Street;
-///
-///   @Implements(GeographicArea)
+///   @Implements<GeographicArea>()
+///   @Implements<AdministrativeArea<House>()
 ///   const factory Example.city(String name, int population) = City;
 /// }
 /// ```
 ///
 /// Note: You need to make sure that you comply with the interface requirements
-/// by implementing all the abstract members. If the interface has no members or
-/// just fields you can fulfil the interface contract by adding them in the
-/// constructor of the union type. Keep in mind that if the interface defines a
+/// by implementing all the abstract members. If the mixin has no members or
+/// just fields, you can fulfil the interface contract by adding them in the
+/// constructor of the union type. Keep in mind that if the mixin defines a
 /// method or a getter, that you implement in the class, you need to use the
 /// [Custom getters and methods](#custom-getters-and-methods) instructions.
-class Implements {
-  const Implements(this.type) : stringType = null;
+class Implements<T> {
+  const Implements();
+}
 
-  const Implements.fromString(this.stringType) : type = null;
+/// Allows a Freezed class to extend another class.
+///
+/// ```dart
+/// @freezed
+/// abstract class Example with _$Example {
+///   @Extends<BaseClass>()
+///   const factory Example(String param) = _Example;
+/// }
+/// ```
+///
+/// By default, Freezed calls the default constructor with no argument.
+///
+/// If you want to use a custom super constructor, it is possible to optionally
+/// specify the super constructor to the annotation:
+///
+/// ```dart
+/// @freezed
+/// abstract class Example with _$Example {
+///   @Extends<BaseClass>('super.named(param: param)')
+///   const factory Example(String param) = _Example;
+/// }
+/// ```
+///
+/// Note: You need to make sure that you comply with the interface requirements
+/// by implementing all the abstract members. If the mixin has no members or
+/// just fields, you can fulfil the interface contract by adding them in the
+/// constructor of the union type. Keep in mind that if the mixin defines a
+/// method or a getter, that you implement in the class, you need to use the
+/// [Custom getters and methods](#custom-getters-and-methods) instructions.
+class Extends<T> {
+  const Extends([this.superCtor]);
 
-  final Type? type;
-  final String? stringType;
+  /// The super constructor to use.
+  ///
+  /// Defaults to `super()`.
+  final String? superCtor;
 }
 
 /// Marks a union type to mixin the class [type] or [stringType].
@@ -250,11 +267,8 @@ class Implements {
 /// abstract class Example with _$Example {
 ///   const factory Example.person(String name, int age) = Person;
 ///
-///   @With.fromString('AdministrativeArea<House>')
+///   @With<AdministrativeArea<House>()
 ///   const factory Example.street(String name) = Street;
-///
-///   @With(GeographicArea)
-///   const factory Example.city(String name, int population) = City;
 /// }
 /// ```
 ///
@@ -264,13 +278,8 @@ class Implements {
 /// constructor of the union type. Keep in mind that if the mixin defines a
 /// method or a getter, that you implement in the class, you need to use the
 /// [Custom getters and methods](#custom-getters-and-methods) instructions.
-class With {
-  const With(this.type) : stringType = null;
-
-  const With.fromString(this.stringType) : type = null;
-
-  final Type? type;
-  final String? stringType;
+class With<T> {
+  const With();
 }
 
 /// An annotation used to specify how a union type will be serialized.
